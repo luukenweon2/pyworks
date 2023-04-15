1f8713316d1c1f734f6c6a58b8a9f539b69d847a
ken luu <avfrdt@gmail.com>
2023-04-15 01:59:25 +0000

    모률 불러쓸때 기존모률 전역변수 생략

--- py230414/py230414_02.py
+++ py230414/py230414_02.py
@@ -3,5 +3,6 @@ class Dog:
         self.name=name
 
 dog1 = Dog("사랑이")
-                 
-print("강아지이름은", dog1.name)
+
+if __name__ == "__main__":
+    print("강아지이름은", dog1.name)
--- py230414/py230414_04.py
+++ py230414/py230414_04.py
@@ -1,5 +1,8 @@
 from py230414_03 import AP
+from py230414_02 import Dog
 
 a=AP("제주항공")
+b=Dog("멍뭉이")
 
 print(a.brand)
+print(b.name)
